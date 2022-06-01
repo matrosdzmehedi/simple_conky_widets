3fcf3732af4d5c3f7551b414d34f2b1bd29f9922
Matrosdzmehedi <matrosdzmehedi111@gmail.com>
2022-06-01 15:33:10 +0600

    Update matrosdz.sh
    added directory check

--- matrosdz.sh
+++ matrosdz.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+DIR="$HOME/.fonts/"
+if [ -d "$DIR" ]; then
+   echo "'$DIR' found "
+else
+   mkdir -vp $HOME/.fonts
+fi
+
 mv -f *.ttf $HOME/.fonts
 
 mkdir matrosdz;
 
 mv clock dayname sysinfo date LICENSE README.md ./matrosdz ;
 
-mv matrosdz $HOME/.conky
-
-
+mv matrosdz/ $HOME/.conky/
