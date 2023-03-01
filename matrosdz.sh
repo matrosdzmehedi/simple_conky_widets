8c7e0980faa700dcce10629eca306145dd3d304c
Matrosdzmehedi <matrosdzmehedi111@gmail.com>
2023-03-01 11:08:03 +0600

    dynamically set wifi name

--- matrosdz.sh
+++ matrosdz.sh
@@ -11,6 +11,10 @@ mv -f *.ttf $HOME/.fonts
 
 mkdir matrosdz;
 
+# setting wifi name dynamically 
+wifi=$(ip r | grep default | awk '{print $5}')
+sed -i "s/wlp9s0/$wifi/"  sysinfo
+
 mv clock dayname sysinfo date LICENSE README.md ./matrosdz ;
 
 mv matrosdz/ $HOME/.conky/
